94cc650c8eae5c44e07d28c4363f9b2355667373
uditar <rethareu@tcd.ie>
2019-05-30 22:41:52 +0100

    test project

---
+++ MyApp.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyApp.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyApp.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e970f73e-5513-4cc5-a4ea-91275b00cbdb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MyApp.UnitTest/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyApp;
+using Moq;
+using System.Web.Mvc;
+using System.Net;
+
+namespace MyApp.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Controllers.ToDoModelsController controller = new Controllers.ToDoModelsController();
+            //change the Id to any existing task ID eg. 3/4/6
+            ActionResult result = controller.DeleteConfirmed(3);
+            Assert.IsInstanceOfType(result, typeof(RedirectToRouteResult));
+
+            RedirectToRouteResult routeResult = result as RedirectToRouteResult;
+            Assert.AreEqual(routeResult.RouteValues["action"], "Index");
+
+        }
+    }
+}
